2562d90cd0f9aca244fb5a0bd38b3a21dfe1086e
liaoyl <liaoyl@my.mail>
2020-05-29 16:46:17 +0800

    chore: 简化

---
+++ build/precache-manifest.3c3a6459dc5262e2cc398b82016651ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5affc753d0f126d27272a721bda3f92a",
+    "url": "/static/media/bg.5affc753.jpg"
+  },
+  {
+    "revision": "47c17fd19d724e162507f2a53341de02",
+    "url": "/static/media/bg.47c17fd1.webp"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c08edd1e77000758396e",
+    "url": "/static/js/main.c08edd1e.chunk.js"
+  },
+  {
+    "revision": "6d6eeb4231d930d93b37",
+    "url": "/static/js/1.6d6eeb42.chunk.js"
+  },
+  {
+    "revision": "c08edd1e77000758396e",
+    "url": "/static/css/main.836bf849.chunk.css"
+  },
+  {
+    "revision": "9b6d090b0db803ec10d833e6ffac67d0",
+    "url": "/index.html"
+  }
+];
